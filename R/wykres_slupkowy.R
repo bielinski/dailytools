e81408c543a72099e3bb21b993370261de6d994e
Jacek Bieliński <jacekbielinski80@gmail.com>
2026-02-21 08:31:21 +0100

    wykres_slupkowy kolory nask i motyw nask

--- R/wykres_slupkowy.R
+++ R/wykres_slupkowy.R
@@ -20,15 +20,15 @@
 #' @export
 #'
 #' @examples
-#' 
+#'
 #' mtcars$gear_fct <- as.factor(mtcars$gear)
 #' wykres_slupkowy(.data = mtcars, column = gear_fct)
-#' 
-wykres_slupkowy <- function(.data, 
+#'
+wykres_slupkowy <- function(.data,
                             column,
                             cross_var = NULL, #TODO: wykorzystanie argumentu dla facet_wrap()
                             weight = NULL,
-                            fill_color = 'steelblue',
+                            fill_color = kolory_unipolar_5[5],
                             title = NULL,
                             subtitle = NULL,
                             caption = NULL,
@@ -37,51 +37,52 @@ wykres_slupkowy <- function(.data,
                             co_flip = FALSE, # czy odwrócić wykres
                             rev_values = FALSE,
                             highlighted_cat = NULL,
-                            highlight_fill = NULL,
+                            highlight_fill = kolory_highlight$pomarancz,
+                            bars_width = 0.8, #TODO: implementacja argumentu określającego szerokośc słupków
                             ...) {
   require(dplyr)
   require(tidyr)
   require(ggplot2)
   require(stringr)
-  
-  
+
+
   if(is.null(cross_var)){
-    n_valid <- .data %>% 
-      drop_na({{column}}) %>% 
-      count({{column}}, ...) %>% 
-      #filter(!is.na(values)) %>% 
-      summarize(n = round(sum(n), digits = 0))  
-    
-    df_tmp <- .data %>% 
-      drop_na({{column}}) %>% 
-      count({{column}}) %>% 
-      mutate(prop = 100*(n/sum(n))) %>% 
+    n_valid <- .data %>%
+      drop_na({{column}}) %>%
+      count({{column}}, ...) %>%
+      #filter(!is.na(values)) %>%
+      summarize(n = round(sum(n), digits = 0))
+
+    df_tmp <- .data %>%
+      drop_na({{column}}) %>%
+      count({{column}}) %>%
+      mutate(prop = 100*(n/sum(n))) %>%
       rename(value = {{column}})
-    
+
   } else { #TODO: do poprawy ten kod
-    n_valid <- .data %>% 
-      select({{column}}, cross_var) %>% 
-      drop_na() %>% 
-      count({{column}}, cross_var) %>% 
-      summarize(n = round(sum(n), digits = 0)) 
-    
-    df_tmp <- .data %>% 
-      group_by(cross_var) %>% 
-      drop_na({{column}}) %>% 
-      count({{column}}) %>% 
-      mutate(prop = 100*(n/sum(n))) %>% 
+    n_valid <- .data %>%
+      select({{column}}, cross_var) %>%
+      drop_na() %>%
+      count({{column}}, cross_var) %>%
+      summarize(n = round(sum(n), digits = 0))
+
+    df_tmp <- .data %>%
+      group_by(cross_var) %>%
+      drop_na({{column}}) %>%
+      count({{column}}) %>%
+      mutate(prop = 100*(n/sum(n))) %>%
       rename(value = {{column}})
   }
-  
-  
+
+
   # 	print(n_valid)
   #  print(df_tmp)
-  
-  
-  the_plot <- df_tmp %>% 
+
+
+  the_plot <- df_tmp %>%
     ggplot(aes(x = value, y = prop))
-  
-  
+
+
   #mechanizm wyróżniania jednej z kategorii na wykresie
   if(!is.null(highlighted_cat)){
     the_plot <- the_plot +
@@ -89,30 +90,30 @@ wykres_slupkowy <- function(.data,
         fill=factor(ifelse(value==highlighted_cat,
                            highlight_fill,
                            fill_color))
-      ), 
+      ),
       show.legend = FALSE) +
-      scale_fill_manual(name = vars(value), values=c(granatowy_paleta[8],granatowy))
+      scale_fill_manual(name = vars(value), values=c(fill_color, highlight_fill))
   } else {
     the_plot <- the_plot +
       geom_col(fill = fill_color)
   }
-  
-  
+
+
   if(rev_values == TRUE){
     the_plot <- the_plot +
       scale_x_discrete(
         limits = rev, #odwrócenie kolejności itemów na wykresie (oś pozioma)
-        labels = function(x) 
+        labels = function(x)
           str_wrap(x,
                    width = axix_x_labs_wrap))
   } else {
     the_plot <- the_plot +
-      scale_x_discrete(labels = function(x) 
+      scale_x_discrete(labels = function(x)
         str_wrap(x,
                  width = axix_x_labs_wrap))
   }
-  
-  
+
+
   the_plot <- the_plot +
     scale_y_continuous(expand = expansion(mult = c(0, expand_top))) +
     labs(
@@ -122,46 +123,52 @@ wykres_slupkowy <- function(.data,
     ) +
     ylab('%')+
     xlab('')
-  
+
   # obsługa argumenty co_flip
   if(co_flip){
-    the_plot <- the_plot + 
+    the_plot <- the_plot +
       coord_flip() +
       geom_text(
         aes(
           label = paste0(sprintf("%0.1f",
-                                 round(prop, 
+                                 round(prop,
                                        digits = 1)),
                          '%')
         ),
         hjust=-0.2, size=3.5) #TODO obsłużyć zmianę tych parametrów dla coord_flip
   } else {
-    the_plot <- the_plot + 
+    the_plot <- the_plot +
       geom_text(
         aes(
           label = paste0(sprintf("%0.1f",
-                                 round(prop, 
+                                 round(prop,
                                        digits = 1)),
                          '%')
         ),
         vjust=-0.3, size=3.5) #TODO obsłużyć zmianę tych parametrów dla coord_flip
   }
-  
+
   if(!is.null(cross_var)){ #TODO: do poprawy ten kod
-    the_plot <- the_plot + 
-      facet_wrap(vars(cross_var), ncol = 2, scales = 'free')  
+    the_plot <- the_plot +
+      facet_wrap(vars(cross_var), ncol = 2, scales = 'free')
   }
-  
-  
+
+
   the_plot <- the_plot +
-    theme_minimal()
-  
+    theme_nask()
+
   if(!co_flip){
     the_plot <- the_plot +
       theme(
         axis.title.x = element_blank()
       )
-  } 
-  
-  the_plot
+  }
+
+  the_plot +
+    theme(
+      #legend.position = legend.position,
+      legend.title = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank()
+      )
 }
